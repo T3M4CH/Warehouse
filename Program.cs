1ff168ad6c816bb2142db76a5f351a96c01a81d2
Artyom <picheneg.epta@gmail.com>
2025-03-19 16:40:00 +0300

    DI
    Чет большой Program.cs мб вынесу бинды в отдельный класс

--- Program.cs
+++ Program.cs
@@ -6,8 +6,14 @@ using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 using Warehouse.Data;
 using Warehouse.Entities;
+using Warehouse.Services;
+using Warehouse.Services.Interfaces;
 using Warehouse.Token;
 using Warehouse.Token.Interfaces;
+using Warehouse.UnitOfWork;
+using Warehouse.UnitOfWork.Interfaces;
+using WarehouseApi.Repositories;
+using WarehouseApi.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,37 +24,43 @@ builder.Services.AddDbContext<DataContext>
     (options => options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnection")));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-{
-    options.Password.RequireDigit = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequiredLength = 5;
-})
-.AddEntityFrameworkStores<DataContext>()
-.AddDefaultTokenProviders();
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 5;
+    })
+    .AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
     {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true
-    };
-});
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true
+        };
+    });
 
 builder.Services.AddAuthorization();
 
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IContainerRepository, ContainerRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IWarehouseRepository, WarehouseRepository>();
+builder.Services.AddScoped<IContainerService, ContainerService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
--- UnitOfWork/UnitOfWork.cs
+++ UnitOfWork/UnitOfWork.cs
@@ -1,6 +1,33 @@
-﻿namespace Warehouse.UnitOfWork;
+﻿using Warehouse.Data;
+using Warehouse.UnitOfWork.Interfaces;
+using WarehouseApi.Repositories.Interfaces;
 
-public class UnitOfWork
+namespace Warehouse.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
 {
-    
+    private readonly DataContext _context;
+
+    public UnitOfWork
+    (
+        DataContext context,
+        IProductRepository productRepository,
+        IContainerRepository containerRepository,
+        IWarehouseRepository warehouseRepository
+    )
+    {
+        _context = context;
+        ProductRepository = productRepository;
+        ContainerRepository = containerRepository;
+        WarehouseRepository = warehouseRepository;
+    }
+
+    public async Task CommitAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public IProductRepository ProductRepository { get; }
+    public IContainerRepository ContainerRepository { get; }
+    public IWarehouseRepository WarehouseRepository { get; }
 }
